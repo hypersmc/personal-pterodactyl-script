05efefba46ff04509764c190d346a7084a75f909
JumpWatch <mhypers@gmail.com>
2020-09-24 12:17:10 +0200

    finished product.

--- FIRE.sh
+++ FIRE.sh
@@ -36,9 +36,9 @@ setup(){
     exit 5
   fi
   output "before we start you need to type following:"
-  output "FQDN (just the http or https no ip):"
+  output "FQDN (just the http:// or https:// no ip. all lowercase):"
   read FQDNB
-  output "FQDN (without https or http):"
+  output "FQDN (without https:// or http://):"
   read FQDN
   output "Email:"
   read Email
@@ -116,13 +116,23 @@ composerandenv(){
   /usr/local/bin/composer install --no-dev --optimize-autoloader
   php artisan key:generate --force
   php artisan p:environment:setup -n --author=$Email --url=$FQDNB$FQDN --timezone=America/New_York --cache=redis --session=database --queue=redis --redis-host=127.0.0.1 --redis-pass= --redis-port=6379
-  php artisan p:environment:database --host=127.0.0.1 --port=3306 --database=panel --username=pterodactyl --password=$password
-  php artisan p:environment:database --host=$dbip --port=3306 --database=$panel --username=$dbname --password=$dbpass
+  php artisan p:environment:database --host=$dbip --port=3306 --database=panel --username=$dbname --password=$dbpass
   output "select your mail method"
   php artisan p:environment:mail
   output "if everything is correct everything should be working now!"
   php artisan migrate --seed --force
-  php artisan p:user:make --email=$Email --admin=1
+
+
+  output "please type Admin user username:"
+  read AUsername
+  output "please type Admin user first-name:"
+  read FirstName
+  output "please type Admin user last-name:"
+  read LastName
+  output "please type Admin user password:"
+  read Apassword
+
+  php artisan p:user:make --email=$Email --admin=1 --username=$AUsername --name-first=$FirstName --name-last=$LastName --password=$Apassword
   output "making permissions for webserver"
   chown -R www-data:www-data * /var/www/pterodactyl
   output "doing crontab"
@@ -148,11 +158,145 @@ ExecStart=/usr/bin/php /var/www/pterodactyl/artisan queue:work --queue=high,stan
 [Install]
 WantedBy=multi-user.target
 EOF
+  sudo systemctl enable --now redis-server
+  sudo systemctl enable --now pteroq.service
   webserversetup
 }
 
 webserversetup(){
-  warning "You have to make webservice file yourself."
+  output "Removing default nginx config"
+  rm -rf /etc/nginx/sites-enabled/default
+  if [ "$FQDNB" == "https://" ]; then
+    output "Https ssl key is being ,made"
+    sudo add-apt-repository ppa:certbot/certbot
+    sudo apt update
+    sudo apt install certbot
+    systemctl stop apache
+    service nginx stop
+    certbot certonly --standalone --email "$Email" --agree-tos -d "$FQDN" --non-interactive
+    service nginx restart
+    output "Https webserver is being made"
+    echo '
+server {
+    listen 80;
+    server_name '"$FQDN"';
+    return 301 https://$server_name$request_uri;
+}
+
+server {
+    listen 443 ssl http2;
+    server_name '"$FQDN"';
+
+    root /var/www/pterodactyl/public;
+    index index.php;
+
+    access_log /var/log/nginx/pterodactyl.app-access.log;
+    error_log  /var/log/nginx/pterodactyl.app-error.log error;
+
+    # allow larger file uploads and longer script runtimes
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    # SSL Configuration
+    ssl_certificate /etc/letsencrypt/live/'"$FQDN"'/fullchain.pem;
+    ssl_certificate_key /etc/letsencrypt/live/'"$FQDN"'/privkey.pem;
+    ssl_session_cache shared:SSL:10m;
+    ssl_protocols TLSv1.2 TLSv1.3;
+    ssl_ciphers "ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384";
+    ssl_prefer_server_ciphers on;
+
+    # See https://hstspreload.org/ before uncommenting the line below.
+    # add_header Strict-Transport-Security "max-age=15768000; preload;";
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Robots-Tag none;
+    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header X-Frame-Options DENY;
+    add_header Referrer-Policy same-origin;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+        include /etc/nginx/fastcgi_params;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+' | sudo -E tee /etc/nginx/sites-available/pterodactyl.conf >/dev/null 2>&1
+    sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+    systemctl restart nginx
+    output "Https webserver done."
+  elif [ "$FQDNB" == "http://" ]; then
+    systemctl stop apache
+        echo '
+server {
+    listen 80;
+    server_name '"$FQDN"';
+
+    root /var/www/pterodactyl/public;
+    index index.html index.htm index.php;
+    charset utf-8;
+
+    location / {
+        try_files $uri $uri/ /index.php?$query_string;
+    }
+
+    location = /favicon.ico { access_log off; log_not_found off; }
+    location = /robots.txt  { access_log off; log_not_found off; }
+
+    access_log off;
+    error_log  /var/log/nginx/pterodactyl.app-error.log error;
+
+    # allow larger file uploads and longer script runtimes
+    client_max_body_size 100m;
+    client_body_timeout 120s;
+
+    sendfile off;
+
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+\.php)(/.+)$;
+        fastcgi_pass unix:/run/php/php7.2-fpm.sock;
+        fastcgi_index index.php;
+        include fastcgi_params;
+        fastcgi_param PHP_VALUE "upload_max_filesize = 100M \n post_max_size=100M";
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param HTTP_PROXY "";
+        fastcgi_intercept_errors off;
+        fastcgi_buffer_size 16k;
+        fastcgi_buffers 4 16k;
+        fastcgi_connect_timeout 300;
+        fastcgi_send_timeout 300;
+        fastcgi_read_timeout 300;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+}
+' | sudo -E tee /etc/nginx/sites-available/pterodactyl.conf >/dev/null 2>&1
+    sudo ln -s /etc/nginx/sites-available/pterodactyl.conf /etc/nginx/sites-enabled/pterodactyl.conf
+    systemctl restart nginx
+    output "Http webserver done."
+  fi
   installdaemonakanode
 }
 
@@ -192,6 +336,7 @@ EOF
   coreinfo
 }
 coreinfo(){
+  output ""
   output "your website link is: $FQDNB$FQDN"
   output ""
   output "your database credentials:"
@@ -200,10 +345,12 @@ coreinfo(){
   output "DBUser: $dbname"
   output "DBPass: $dbpass"
   output ""
-  output "login:"
-  output "email: $Email"
-  output "username: notset"
-  output "password: notset"
+  output "Login:"
+  output "Email: $Email"
+  output "Username: $AUsername"
+  output "First name: $FirstName"
+  output "Last name: $LastName"
+  output "Password: $Apassword"
 }
 #yeet stuff
 setup
